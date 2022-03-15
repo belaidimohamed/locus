73544510a8ed6a678fb3cb916d6b9f9eda5d9eac
Hamza Ghenimi <49754305+hamzagh1998@users.noreply.github.com>
2022-03-15 19:24:01 +0100

    Delete server/dist directory

--- server/dist/v1/utils/errorHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-function errorHandler(fn) {
-    return (req, res, next) => Promise.resolve(fn(req, res, next))
-        .catch(next);
-}
-exports.errorHandler = errorHandler;
-;
